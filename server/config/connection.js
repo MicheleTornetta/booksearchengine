8021930c5f70ec807e488e6c73494f4fee3b9702
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-15 00:23:57 -0500

    fixed wrong connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.y5shcl2.mongodb.net/?retryWrites=true&w=majority/googlebooks`, {
+mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.y5shcl2.mongodb.net/?retryWrites=true/googlebooks`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
