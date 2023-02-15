c11a687ca8e0a54cab01f87cf9c563582b3f9c65
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-02-15 00:31:13 -0500

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.y5shcl2.mongodb.net/?retryWrites=true/googlebooks`, {
+mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.y5shcl2.mongodb.net/googlebooks?retryWrites=true`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
